d37a6a40b84dfdf742c323c1482a5ffdabdec7c8
ger <68071939+gerzarko@users.noreply.github.com>
2023-08-14 14:57:17 -0400

    add internationalization

--- src/lib/PasswordReset.tsx
+++ src/lib/PasswordReset.tsx
@@ -1,18 +1,25 @@
-import { supabase } from './supabaseClient'
-import { Component, createSignal } from 'solid-js'
-import { getLangFromUrl, useTranslations } from '../i18n/utils';
+import { supabase } from "./supabaseClient";
+import { Component, createSignal } from "solid-js";
+import { getLangFromUrl, useTranslations } from "../i18n/utils";
 
 const lang = getLangFromUrl(new URL(window.location.href));
 const t = useTranslations(lang);
 
 export const PasswordReset: Component = () => {
-    const [loading, setLoading] = createSignal(false)
-    const [email, setEmail] = createSignal('')
+  const [loading, setLoading] = createSignal(false);
+  const [email, setEmail] = createSignal("");
 
-
-    return (
-        <div class="bg-primaryBackground">
-            <p class="text-sm text-text1 dark:text-text1-DM"> {t('messages.forgotPassword')} <a class="text-link2 hover:underline dark:text-link2-DM" href={`/${lang}/password/request`}>{t('buttons.reset')}</a></p>
-        </div>
-    )
-}
+  return (
+    <div class="bg-primaryBackground">
+      <p class="text-sm text-text1 dark:text-text1-DM">
+        {t("messages.forgotPassword")}
+        <a
+          class="text-link2 hover:underline dark:text-link2-DM"
+          href={`/${lang}/password/request`}
+        >
+          {t("buttons.reset")}
+        </a>
+      </p>
+    </div>
+  );
+};
