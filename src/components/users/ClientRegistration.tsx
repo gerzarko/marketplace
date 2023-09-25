56bd57b51839c6ff743c879be560466057f89975
German Zarkovich <68071939+gerzarko@users.noreply.github.com>
2023-09-25 17:15:37 -0400

    add svgs in div display in flex

--- src/components/users/ClientRegistration.tsx
+++ src/components/users/ClientRegistration.tsx
@@ -13,6 +13,8 @@ import { getLangFromUrl, useTranslations } from "../../i18n/utils";
 const lang = getLangFromUrl(new URL(window.location.href));
 const t = useTranslations(lang);
 
+const crossMark = "../../assets/crossMark.svg";
+
 async function postFormData(formData: FormData) {
   const response = await fetch("/api/clientProfileSubmit", {
     method: "POST",
@@ -194,6 +196,24 @@ export const ClientRegistration: Component = () => {
     }
   });
 
+  function renderTickOrCross(e:any):any {
+
+    if(e.target.value.length > 0){
+        
+      return(
+            <h1>test</h1>
+            )
+        
+    }else{
+        return(
+            <h1>test2</h1>
+              )
+    }
+
+      
+  }
+
+
   function submit(e: SubmitEvent) {
     e.preventDefault();
     // this might not be the best way to do this but it works and we can also have more control over the form input data
@@ -223,7 +243,7 @@ export const ClientRegistration: Component = () => {
               {t("formLabels.firstName")}:
             </label>
             <div class="group flex items-center relative mr-2">
-              <svg class="peer w-4 h-4 border-2 bg-icon1 dark:bg-background1-DM fill-iconbg1 dark:fill-iconbg1-DM  border-border1 dark:border-none rounded-full" version="1.1" xmlns="http://www.w3.org/2000/svg"
+              <svg class="peer w-4 h-4 border-2 bg-icon1 dark:bg-background1-DM fill-iconbg1 dawwrk:fill-iconbg1-DM  border-border1 dark:border-none rounded-full" version="1.1" xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 512 512">
                 <g>
                   <path d="M255.992,0.008C114.626,0.008,0,114.626,0,256s114.626,255.992,255.992,255.992
@@ -238,23 +258,30 @@ export const ClientRegistration: Component = () => {
                 </g>
               </svg>
 
-              <span
-                class="peer-hover:visible transition-opacity bg-background2 dark:bg-background2-DM text-sm text-ptext2 dark:text-ptext2-DM rounded-md absolute 
-                md:translate-x-1/4 -translate-x-full -translate-y-2/3 md:translate-y-0 invisible m-4 mx-auto p-2 w-48">
-                {t("toolTips.firstName")}
-              </span>
+            
 
             </div>
           </div>
+
+          <div class="flex flex-row">
+
           <input
             type="text"
             id="FirstName"
             name="FirstName"
             class="rounded w-full mb-4 px-1 focus:border-highlight1 dark:focus:border-highlight1-DM border focus:border-2 border-inputBorder1 dark:border-inputBorder1-DM focus:outline-none bg-background1 dark:bg-background2-DM text-ptext1 dark:text-ptext2-DM"
             required
-          />
-        </div>
+            onchange={(e) =>(renderTickOrCross(e))}
+         />
 
+         <div>
+            <div class="group flex items-center relative mr-2">
+                <svg class="peer w-4 h-4 border-2 bg-icon1 dark:bg-background1-DM fill-iconbg1 dark:fill-iconbg1-DM  border-border1 dark:border-none rounded-full"enable-background="new 0 0 128 128" viewBox="0 0 128 128" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><linearGradient id="a" gradientUnits="userSpaceOnUse" x1="64" x2="64" y1="2.9028" y2="122.12"><stop offset="0" stop-color="#ff5252"/><stop offset=".4455" stop-color="#ee3030"/><stop offset="1" stop-color="#d50000"/></linearGradient><path d="m82.35 65.42c-.78-.78-.78-2.05 0-2.83l38.15-38.11c4.67-4.69 4.67-12.28 0-16.97s-12.23-4.69-16.9 0l-38.18 38.14c-.78.78-2.05.78-2.83 0l-38.1-38.15c-4.69-4.67-12.29-4.67-16.98 0s-4.69 12.23 0 16.9l38.14 38.17c.78.78.78 2.05 0 2.83l-38.15 38.11c-4.67 4.69-4.67 12.28 0 16.97s12.23 4.69 16.9 0l38.17-38.14c.78-.78 2.05-.78 2.83 0l38.11 38.15c4.69 4.67 12.28 4.67 16.97 0s4.69-12.23 0-16.9z" fill="url(#a)"/><path d="m16 7c2.41 0 4.67.93 6.36 2.62l38.11 38.15c.94.95 2.2 1.47 3.54 1.47 1.33 0 2.59-.52 3.53-1.46l38.18-38.14c1.69-1.7 3.94-2.63 6.33-2.63s4.64.93 6.33 2.63c3.5 3.51 3.5 9.23.01 12.73l-38.16 38.1c-1.95 1.95-1.95 5.12 0 7.07l38.14 38.18c1.7 1.69 2.63 3.94 2.63 6.33s-.93 4.64-2.63 6.33-3.96 2.63-6.37 2.63-4.67-.93-6.36-2.62l-38.11-38.16c-.94-.95-2.2-1.47-3.54-1.47-1.33 0-2.59.52-3.53 1.46l-38.18 38.14c-1.69 1.7-3.94 2.63-6.33 2.63s-4.64-.93-6.33-2.63c-3.5-3.51-3.5-9.23-.01-12.73l38.15-38.11c1.95-1.95 1.95-5.12 0-7.07l-38.13-38.17c-1.7-1.69-2.63-3.94-2.63-6.33s.93-4.64 2.63-6.33 3.96-2.62 6.37-2.62m0-3c-3.07 0-6.14 1.17-8.49 3.5-4.69 4.67-4.69 12.23 0 16.9l38.14 38.17c.78.78.78 2.05 0 2.83l-38.15 38.11c-4.67 4.69-4.67 12.28 0 16.97 2.33 2.34 5.39 3.51 8.45 3.51s6.12-1.17 8.45-3.51l38.17-38.14c.39-.39.9-.59 1.41-.59s1.02.2 1.41.59l38.11 38.15c2.34 2.33 5.41 3.5 8.49 3.5s6.14-1.17 8.49-3.5c4.69-4.67 4.69-12.23 0-16.9l-38.13-38.17c-.78-.78-.78-2.05 0-2.83l38.15-38.11c4.67-4.69 4.67-12.28 0-16.97-2.33-2.34-5.39-3.51-8.45-3.51s-6.12 1.17-8.45 3.51l-38.18 38.14c-.39.39-.9.59-1.41.59s-1.02-.2-1.41-.59l-38.11-38.15c-2.35-2.33-5.42-3.5-8.49-3.5z" fill="#424242" opacity=".2"/></svg>
+                <svg  class="peer w-4 h-4 border-2 bg-icon1 dark:bg-background1-DM fill-iconbg1 dark:fill-iconbg1-DM  border-border1 dark:border-none rounded-full"exmlns="http://www.w3.org/2000/svg"><path d="m4.94960124 7.88894106-1.91927115-1.91927115c-.29289322-.29289321-.76776696-.29289321-1.06066018 0-.29289321.29289322-.29289321.76776696 0 1.06066018l2.5 2.5c.31185072.31185071.82415968.28861186 1.10649605-.05019179l5.00000004-6c.265173-.31820767.22218-.7911312-.0960277-1.05630426s-.7911312-.22218001-1.05630426.09602766z" fill="#00FF00"/></svg>
+            </div>
+         </div>
+</div>
+         </div>
         <div class="">
           <div class="flex flex-row justify-between">
             <label for="LastName" class="text-ptext1 dark:text-ptext1-DM">
